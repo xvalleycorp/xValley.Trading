85d7015b65995165c8c76d8b66728c74f4f1409f
Tung Vu <tungvm1.fsoft@gmail.com>
2023-07-14 22:40:27 +0700

    Adding config for the Data processing module

--- xValley.Trading.DataProcessing/Program.cs
+++ xValley.Trading.DataProcessing/Program.cs
@@ -1,14 +1,21 @@
-﻿using xValley.Trading.DataProcessing.Processors;
+﻿using System.Configuration;
+using xValley.Trading.DataProcessing.Processors;
 
 Console.Write("Please enter the currency pair: ");
 var currencyPair = Console.ReadLine();
 var fileName = string.Format("{0}_D1.csv", currencyPair);
+
+// Get config from the App.config 
+var tradingDataDir = ConfigurationManager.AppSettings["TradingDataDir"];
 Console.WriteLine("Fetching Data for {0} from {1}!", currencyPair, fileName);
-var csvPath = string.Format(@"D:\Labs\ML.NET\TradingML\xValley.Trading\TradingData\{0}\{1}", currencyPair, fileName);
+
+// Load data from csv file
+var csvPath = string.Format(@"{0}\{1}\{2}", tradingDataDir, currencyPair, fileName);
 var data = CsvDataProcessor.LoadDataFromCsv(csvPath);
+
 // Map with Indicator result
 var mappedResult = CsvDataProcessor.IndicatorMapping(data);
-var outputPath = string.Format(@"D:\Labs\ML.NET\TradingML\xValley.Trading\TradingData\Transformed\{0}", fileName);
+var outputPath = string.Format(@"{0}\Transformed\{1}", tradingDataDir, fileName);
 Console.WriteLine(@"Writing Data to '{0}'",outputPath);
 CsvDataProcessor.WriteToCsv(mappedResult.Skip(34).ToList(), outputPath);
 Console.WriteLine("Done!");
